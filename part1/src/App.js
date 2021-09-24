0cdf64d5ba895dc12601ef1017758dc37c7aab5c
des-ant <57375593+des-ant@users.noreply.github.com>
2021-09-24 22:14:09 +1000

    Refactored Content component and created Part component

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,11 +6,21 @@ const Header = (props) => {
   )
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
       {props.part} {props.exercise}
     </p>
+  )
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} exercise={props.exercises[0]} />
+      <Part part={props.parts[1]} exercise={props.exercises[1]} />
+      <Part part={props.parts[2]} exercise={props.exercises[2]} />
+    </div>
   )
 };
 
@@ -32,9 +42,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
