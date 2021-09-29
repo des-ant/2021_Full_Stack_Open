509cd8cb00d20c38dbcdb1edd12bd8586e024cac
des-ant <57375593+des-ant@users.noreply.github.com>
2021-09-29 23:40:15 +1000

    Created customer feedback application, ex 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const Header = ({ title }) => <h1>{title}</h1>;
 
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
@@ -8,28 +8,28 @@ const Button = ({ onClick, text }) => (
   </button>
 );
 
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0);
+const Display = ({text, counter}) => <p>{text} {counter}</p>;
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+const App = (props) => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const incrementGoodCount = () => setGood(good + 1);
+  const incrementNeutralCount = () => setNeutral(neutral + 1);
+  const incrementBadCount = () => setBad(bad + 1);
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />
+      <Header title='give feedback' />
+      <Button onClick={incrementGoodCount} text='good' />
+      <Button onClick={incrementNeutralCount} text='neutral' />
+      <Button onClick={incrementBadCount} text='bad' />
+      <Header title='statistics' />
+      <Display text='good' counter={good} />
+      <Display text='neutral' counter={neutral} />
+      <Display text='bad' counter={bad} />
     </div>
   )
 };
