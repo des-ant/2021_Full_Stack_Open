9dc3106facd86155a4083016fc669ed1afa4f9f0
des-ant <57375593+des-ant@users.noreply.github.com>
2021-09-30 12:35:19 +1000

    Changed app to display stats only when feedback is gathered - ex 1.9

--- part1/src/App.js
+++ part1/src/App.js
@@ -12,27 +12,38 @@ const Display = ({text, counter, isPercentage}) => (
   <p>{text} { isNaN(counter) ? 0 : counter} { isPercentage ? '%' : '' }</p>
 );
 
-const Statistics = ({ good, neutral, bad, total, average, positive }) => (
-  <div>
-    <Display text='good' counter={good} />
-    <Display text='neutral' counter={neutral} />
-    <Display text='bad' counter={bad} />
-    <Display text='all' counter={total} />
-    <Display text='average' counter={average} />
-    <Display text='positive' counter={positive} isPercentage={true} />
-  </div>
-);
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  if (total === 0) {
+    return (
+      <div>
+        No feeback given
+      </div>
+    )
+  }
+  return (
+    <div>
+      <Display text='good' counter={good} />
+      <Display text='neutral' counter={neutral} />
+      <Display text='bad' counter={bad} />
+      <Display text='all' counter={total} />
+      <Display text='average' counter={average} />
+      <Display text='positive' counter={positive} isPercentage={true} />
+    </div>
+  )
+};
 
 const App = (props) => {
-  // save clicks of each button to its own state
+  // Save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  // Variables to calculate statistics
   const total = good + neutral + bad;
   const average = (good - bad) / total;
   const positive = (good / total) * 100;
 
+  // Functions to update state
   const incrementGoodCount = () => setGood(good + 1);
   const incrementNeutralCount = () => setNeutral(neutral + 1);
   const incrementBadCount = () => setBad(bad + 1);
