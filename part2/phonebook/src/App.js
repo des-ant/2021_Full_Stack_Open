4816b8a0520fb4df6a48debd016d81377adee6c4
des-ant <57375593+des-ant@users.noreply.github.com>
2021-10-04 18:00:49 +1100

    Extended app by allowing users to add phone numbers -ex2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567',
+    }
   ]);
   const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
 
   const addPerson = (event) => {
     // Prevent page reload
@@ -17,16 +21,23 @@ const App = () => {
     // Create new person
     const personObject = {
       name: newName,
+      number: newNumber,
     };
     // Add person to persons list without mutating list
     setPersons(persons.concat(personObject));
-    // Clear input field
+    // Clear input fields
     setNewName('');
+    setNewNumber('');
   }
 
   const handleNameChange = (event) => {
     // Update field value when typing in field
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    // Update field value when typing in field
+    setNewNumber(event.target.value);
   }
 
   return (
@@ -41,13 +52,20 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+        />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person =>
-          <li>{person.name}</li>
+          <li>{person.name} {person.number}</li>
         )}
       </ul>
     </div>
